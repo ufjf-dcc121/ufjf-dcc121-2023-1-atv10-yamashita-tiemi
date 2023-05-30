3172714ad3f00969a4db57a3ae689913f29398f7
Victoria <victoria.yamashita@estudante.ufjf.br>
2023-05-30 19:28:04 -0300

    add basic state

--- main.js
+++ main.js
@@ -2,7 +2,16 @@
 const form = document.forms.entrada;
 form.addEventListener('submit', send);
 
+let state = 0;
+
 function send(event) {
     event.preventDefault();
     console.log('Formulário enviado!');
+    state++;
+    update();
+}
+
+function update() {
+    const ol = document.querySelector('ol');
+    ol.innerHTML = `<li>${state}</li>`;
 }
